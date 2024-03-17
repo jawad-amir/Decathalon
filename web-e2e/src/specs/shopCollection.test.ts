c3f12976640a6212c01879963b41bc6cca660554
Jawad Amir <jawad.amir466@gmail.com>
2024-03-17 20:42:45 +0500

    update

--- web-e2e/src/specs/shopCollection.test.ts
+++ web-e2e/src/specs/shopCollection.test.ts
@@ -7,14 +7,17 @@ test.describe("Shop Collection", () => {
     let page: any;
 
     test.beforeEach(async ({ browser }) => {
+        // Open a new page on browser before each test
         page = await browser.newPage();
     });
 
     test.afterEach(async () => {
+        // Close page after each test
         await page.close();
     });
 
     test("Add items to Bag and Checkout", async () => {
+        // Create a variable to store the Test Data
         const testData1 = {
             page,
             inputAddressField: "Test Address Street Bla Bla bla",
@@ -31,22 +34,31 @@ test.describe("Shop Collection", () => {
         };
 
 
+        test.step('Navigate to the URL', async () => {
         await goToShopCollectionPage(page);
+        });
+
+        test.step('Navigate to Shop Collection/ Items', async () => {
         await clickShopCollection(page);
+        });
+        test.step('Add Items to Bag', async () => {
         await shopPage.clickItem1(page);
         await shopPage.addItemToBag(page);
         await shopPage.clickItem2(page);
         await shopPage.addItemToBag(page);
         await shopPage.clickItem3(page);
         await shopPage.addItemToBag(page);
+        });
+        test.step('Navigate to Bag in Header and Checkout', async () => {
         await shopPage.bagItems(page);
         await shopPage.goToCheckout(page);
+        });
+        test.step('Fill out the Required Information to Checkout', async () => {
         await shopPage.fillPersonalInformationForm(testData);
         await shopPage.fillAddressForm(testData1); 
         await shopPage.selectHomeDelivery(page);
         await shopPage.selectCashOnDelivery(page);
         await shopPage.completeOrder(page);
-
-        
+        });
     });
 });
